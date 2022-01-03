b832ae1ce42cfe997a3d27d096fb440fc3465fc9
SanjithaSingh <sanjitha.singh@aknamed.com>
2022-01-03 17:31:49 +0530

    fix routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,135 +1,28 @@
-var express = require("express");
-var router = express.Router();
-var Campground = require("../models/campground");
-var Comment = require("../models/comment");
-var middleware = require("../middleware");
-var geocoder = require('geocoder');
-var { isLoggedIn, checkUserCampground, checkUserComment, isAdmin, isSafe } = middleware; // destructuring assignment
+const express = require('express');
+const router = express.Router();
+const campgrounds = require('../controllers/campgrounds');
+const catchAsync = require('../utils/catchAsync');
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
-// Define escapeRegex function for search feature
-function escapeRegex(text) {
-  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
-};
+const Campground = require('../models/campground');
 
-//INDEX - show all campgrounds
-router.get("/", function (req, res) {
-  if (req.query.search && req.xhr) {
-    const regex = new RegExp(escapeRegex(req.query.search), 'gi');
-    // Get all campgrounds from DB
-    Campground.find({ name: regex }, function (err, allCampgrounds) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.status(200).json(allCampgrounds);
-      }
-    });
-  } else {
-    // Get all campgrounds from DB
-    Campground.find({}, function (err, allCampgrounds) {
-      if (err) {
-        console.log(err);
-      } else {
-        if (req.xhr) {
-          res.json(allCampgrounds);
-        } else {
-          res.render("campgrounds/index", { campgrounds: allCampgrounds, page: 'campgrounds' });
-        }
-      }
-    });
-  }
-});
-
-//CREATE - add new campground to DB
-router.post("/", isLoggedIn, isSafe, function (req, res) {
-  // get data from form and add to campgrounds array
-  console.log(req.body);
-  var name = req.body.name;
-  var image = req.body.image;
-  var desc = req.body.description;
-  var author = {
-    id: req.user._id,
-    username: req.user.username
-  }
-  var cost = req.body.cost;
-  var location = req.body.location;
-
-  var newCampground = { name: name, image: image, description: desc, cost: cost, author: author, location: location };
-  // Create a new campground and save to DB
-  Campground.create(newCampground, function (err, newlyCreated) {
-    if (err) {
-      console.log(err);
-    } else {
-      //redirect back to campgrounds page
-      console.log(newlyCreated);
-      res.redirect("/campgrounds");
-    }
-  });
-});
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground))
 
 
-//NEW - show form to create new campground
-router.get("/new", isLoggedIn, function (req, res) {
-  res.render("campgrounds/new");
-});
+router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
-// SHOW - shows more info about one campground
-router.get("/:id", function (req, res) {
-  //find the campground with provided ID
-  Campground.findById(req.params.id).populate("comments").exec(function (err, foundCampground) {
-    if (err || !foundCampground) {
-      console.log(err);
-      req.flash('error', 'Sorry, that campground does not exist!');
-      return res.redirect('/campgrounds');
-    }
-    console.log(foundCampground)
-    //render show template with that campground
-    res.render("campgrounds/show", { campground: foundCampground });
-  });
-});
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, upload.array('image'), validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
-// EDIT - shows edit form for a campground
-router.get("/:id/edit", isLoggedIn, checkUserCampground, function (req, res) {
-  //render edit template with that campground
-  res.render("campgrounds/edit", { campground: req.campground });
-});
-
-// PUT - updates campground in the database
-router.put("/:id", isSafe, function (req, res) {
-  var newData = { name: req.body.name, image: req.body.image, description: req.body.description, cost: req.body.cost, location: req.body.location };
-  Campground.findByIdAndUpdate(req.params.id, { $set: newData }, function (err, campground) {
-    if (err) {
-      req.flash("error", err.message);
-      res.redirect("back");
-    } else {
-      req.flash("success", "Successfully Updated!");
-      res.redirect("/campgrounds/" + campground._id);
-    }
-  });
-});
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
 
 
-// DELETE - removes campground and its comments from the database
-router.delete("/:id", isLoggedIn, checkUserCampground, function (req, res) {
-  Comment.remove({
-    _id: {
-      $in: req.campground.comments
-    }
-  }, function (err) {
-    if (err) {
-      req.flash('error', err.message);
-      res.redirect('/');
-    } else {
-      req.campground.remove(function (err) {
-        if (err) {
-          req.flash('error', err.message);
-          return res.redirect('/');
-        }
-        req.flash('error', 'Campground deleted!');
-        res.redirect('/campgrounds');
-      });
-    }
-  })
-});
 
 module.exports = router;
-
