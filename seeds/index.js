903d8dbe7c4bb314b62d3a98bb57f53bdd0e1fb5
SanjithaSingh <sanjitha.singh@aknamed.com>
2022-01-03 18:47:15 +0530

    set up seed file

--- seeds/index.js
+++ seeds/index.js
@@ -3,18 +3,25 @@ const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+if (process.env.NODE_ENV !== "production") {
+  require('dotenv').config();
+}
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 });
 
 const db = mongoose.connection;
-
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
 });
+
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
